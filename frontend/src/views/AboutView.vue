e109e52fa04ed498fa01de190e169237b29c7248
Aria Seyedahmadi <ariaseyedahmadi.2015@gmail.com>
2022-08-22 16:05:35 +0430

    Adding about page to the header and some content in about page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,6 +1,22 @@
+<script setup>
+	import Header from "../components/Header.vue";
+</script>
+
 <template>
-	<div class="about">
-		<h1>This is an about page</h1>
+	<div class="space-y-3 m-3 lg:mx-auto lg:w-6/12 flex flex-col">
+		<Header />
+
+		<div class="flex flex-col space-y-3 w-full lg:space-x-3">
+			<h3 class="text-xl py-5 border-b w-full">About PepeHub</h3>
+			<div>
+				<img src="@/assets/about" alt="about image" />
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias facere ab recusandae molestias, iste
+					voluptatem eveniet maxime vero aspernatur quasi consectetur repudiandae expedita quam. Quis, sunt
+					laudantium. Impedit, libero quis?
+				</p>
+			</div>
+		</div>
 	</div>
 </template>
 
